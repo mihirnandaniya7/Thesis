7e5122ae1bae4be870b3c0fe42d062d0a9b9f00b
Mihir Nandaniya <nandaniya222@gmail.com>
2026-06-26 10:33:22 +0200

    Expose model API

---
+++ surrogate_thesis/models/__init__.py
@@ -0,0 +1,12 @@
+"""Model classes used by the thesis surrogate pipeline."""
+
+from surrogate_thesis.models.baselines import LinearRegressionBaseline, PersistenceBaseline
+from surrogate_thesis.models.sequence import LSTMRegressor
+from surrogate_thesis.models.transformer import TransformerRegressor
+
+__all__ = [
+    "LinearRegressionBaseline",
+    "PersistenceBaseline",
+    "LSTMRegressor",
+    "TransformerRegressor",
+]

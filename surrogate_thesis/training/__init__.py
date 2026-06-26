48dc521df3882596f2ee8871770e8265a5c1d599
Mihir Nandaniya <nandaniya222@gmail.com>
2026-06-26 10:33:22 +0200

    Expose training API

---
+++ surrogate_thesis/training/__init__.py
@@ -0,0 +1,5 @@
+"""Training and inference public API."""
+
+from surrogate_thesis.training.trainer import ModelArtifacts, fit_model, predict_model
+
+__all__ = ["ModelArtifacts", "fit_model", "predict_model"]
